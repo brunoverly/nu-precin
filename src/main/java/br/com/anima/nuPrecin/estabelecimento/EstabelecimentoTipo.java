252a1e84879c544bfc02d302092e8d44668f4923
brunoverly <bsverly@gmai.com>
2026-05-10 11:50:09 -0300

    adiciona usuario controller e service

---
+++ src/main/java/br/com/anima/nuPrecin/estabelecimento/EstabelecimentoTipo.java
@@ -0,0 +1,7 @@
+package br.com.anima.nuPrecin.estabelecimento;
+
+public enum EstabelecimentoEnum {
+    MERCADO,
+    SUPERMERCADO,
+    ATACADAO
+}
--- src/main/resources/db/migration/V1__create_table_exemplo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE exemplos(
-    id BIGSERIAL PRIMARY KEY,
-    nome varchar(250),
-    numero int,
-    data_horario_criado TIMESTAMP,
-    exemplo_enum varchar(50),
-    ativo boolean
-)
